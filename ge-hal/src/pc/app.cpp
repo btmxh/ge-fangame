d27007cc839e4d39deaf94da258c5e5862c0d4b5
btmxh <ngoduyanh.chip@gmail.com>
2026-01-28 12:10:01 +0900

    fix: fix audio buffering bug

--- ge-hal/src/pc/app.cpp
+++ ge-hal/src/pc/app.cpp
@@ -340,7 +340,11 @@ void App::audio_bgm_play(const std::uint8_t *data, std::size_t len, bool loop) {
   app_impl_instance->bgm = {data, len, 0, loop, true};
 }
 
-void App::audio_bgm_stop() { app_impl_instance->bgm.active = false; }
+void App::audio_bgm_stop() {
+  auto *impl = app_impl_instance.get();
+  impl->bgm.active = false;
+  SDL_ClearAudioStream(impl->audio_stream);
+}
 
 bool App::audio_bgm_is_playing() { return app_impl_instance->bgm.active; }
 
